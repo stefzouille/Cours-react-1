fc97da5fe4f1de977c4c37454baf9a6112e365b6
stefzouille <contact.lokcy@gmail.com>
2022-06-19 14:52:29 +0200

    add route of app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 // notre router
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* on définit les routes params des chemin url */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* si l url ne correspond à aucune route, on affiche la page d'accueil */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
