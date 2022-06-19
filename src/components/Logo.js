d9a718eaca7d9c40b69ed9ba87f0e4a589e6a20b
stefzouille <contact.lokcy@gmail.com>
2022-06-19 15:21:25 +0200

    add minim 4 site vitrine just 4 vue

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,8 @@ const Logo = () => {
   return (
     <div>
       <div className="logo">
-        <img src="" alt="logo react" />
+        {/* les images importées depuis la balise img sont accessible dans "public " */}
+        <img src="./logo192.png" alt="logo react" />
         <h3>React World</h3>
       </div>
     </div>
