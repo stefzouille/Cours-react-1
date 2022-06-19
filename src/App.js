56b85d27611d8880a5a0fee7042f748fb868a613
stefzouille <contact.lokcy@gmail.com>
2022-06-19 12:45:06 +0200

    add presa et struct end

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+// notre router
+import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  );
+};
+
+export default App;
